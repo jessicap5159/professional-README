a985720a0f291ba4c5369b2d9e9c3f31c90b191c
jessicap5159 <jessicaj5159@gmail.com>
2021-03-29 15:26:51 -0700

    created template for readme

--- src/template.js
+++ src/template.js
@@ -8,17 +8,81 @@ const generateTitle = titleText => {
     `
 }
 // create description section
-
+const generateDesc = descText => {
+    if (!descText) {
+        return '';
+    }
+    return `
+        ## Description
+        ${descText}
+    `
+}
 // create table of contents
-
+const generateContents = contentsText => {
+    if (!contentsText) {
+        return '';
+    }
+    return `
+    ## Table of Contents
+    * [Installation](#installation)
+    * [Usage](#usage)
+    * [License](#license)
+    * [Contributing](#contributing)
+    * [Test](#test)
+    * [Questions](#Questions)
+    
+    `
+}
 // create installation section
-
+const generateInstallInstructs = installText => {
+    if (!installText) {
+        return '';
+    }
+    return `
+        ## Installation
+        ${installText}
+    `
+}
 // create usage section
-
-// create license section
+const generateUsage = usageText => {
+    if (!usageText) {
+        return '';
+    }
+    return `
+        ## Usage
+        ${usageText}
+    `
+}
+// license section is in generateMarkdown.js
 
 // create contributions section
-
+const generateContributions = contributionsText => {
+    if (!contributionsText) {
+        return '';
+    }
+    return `
+        ## Contributing
+        ${contributionsText}
+    `
+}
 // create tests section
-
+const generateTestsSection = testsSectionText => {
+    if (!testsSectionText) {
+        return '';
+    }
+    return `
+        ## Tests
+        ${testsSectionText}
+    `
+}
 // create questions section
+    const generateQuestionsSection = questionsSectionText => {
+        if (!testsSquestionsSectionTextctionText) {
+            return '';
+        }
+        return `
+            ## Questions
+            GitHub username: 
+            Please contact me with any questions at: 
+        `
+    }
